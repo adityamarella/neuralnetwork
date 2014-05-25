95a7462e7a975cf47dfa35ee144e8497deb3dbd2
aditya <aditya.marella@gmail.com>
2014-05-24 21:10:52 -0400

    code updated with some fixes and new features

--- NN.m
+++ NN.m
@@ -1,92 +1,78 @@
-function prediction = NN(train, dev, learning_factor, weight_interval_value)
+function prediction = NN(train, dev, learning_factor, max_iterations, nin, nhidden, nout, Wih, Who)
 
-    rows = size(train)(1);
-    cols = size(train)(2);
-
-    train = train(2:rows,:);
-    rows = rows - 1;
-
-    nin = cols - 1;
-    nhidden = nin+1;
-    nout = 2;
-    ntot = nin + nhidden + nout;
-    
-    %initialize weights to uniform random samples between -0.05 to 0.05
-    Wih = randMatrix(nin+1, nhidden, -weight_interval_value, weight_interval_value);
-    Who = randMatrix(nhidden+1, nout, -weight_interval_value, weight_interval_value);
-
-
-    function M = randMatrix(rows, cols, lower, upper)
-        M = lower - (upper-lower)*rand(rows, cols);
-    end
 
     function [Oh Oo] = update_output(X, Oh, Oo)
         %calculating the output for the hidden nodes
         %input -> O(1,nin)
         for i = 1:nhidden
-            Oh(i) = Wih(1,i) + dot(X,Wih(2:nin+1,i));
+            Oh(i) = sigmoid(dot(transpose(Wih(1:nin+1,i)), X));
         end
 
         %calculating the output values for the output nodes
         %input -> O(nin+1, nhidden)
         for i = 1:nout
-            Oo(i) = Who(1,i) + dot(Oh,Who(2:nhidden+1,i));
+            Oo(i) = sigmoid(Who(1,i) + dot(transpose(Who(2:nhidden+1,i)), Oh));
         end
-        
-        ret = 0;
     end
 
     function [Eh Eo] = backpropagate_error(y, Oh, Oo)
         %propagate error for output units
         for i=1:nout
-            o = sigmoid(Oo(i));
+            o = Oo(i);
             Eo(i) = o * (1-o) * (y-o);
         end
 
         %propagate error for hidden units
-        for i=1:nhidden
-            o = sigmoid(Oh(i));
+        for i=1:nhidden+1
+            if i==1
+                o = 1;
+            else
+                o = Oh(i-1);
+            end
             Eh(i) = o * (1-o) * dot(Who(i,:), Eo);
         end
-        ret = 0;
     end
 
     function ret = update_weights(X, Oh, Oo, Eh, Eo)
-        for i=1:nin
+        tmp = [1 Oh(1:nhidden)];
+        for i=1:nin+1
             for j=1:nhidden
-                Wih(i,j) = Wih(i,j) + learning_factor*Eh(j)*X(i);
+                Wih(i,j) = Wih(i,j) + learning_factor*Eh(j+1)*X(i);
             end
         end
-        for i=1:nhidden
+        for i=1:nhidden+1
             for j=1:nout
-                Who(i,j) = Who(i,j) + learning_factor*Eo(j)*Oh(i);
+                Who(i,j) = Who(i,j) + learning_factor*Eo(j)*tmp(i);
             end
         end
-        ret = 0;
     end
 
     function ret = NN_train()
 
+        [rows cols] = size(train);
         Oh = zeros(1,nhidden);
         Oo = zeros(1,nout);
 
-        Eh = zeros(1,nhidden);
+        Eh = zeros(1,nhidden+1);
         Eo = zeros(1,nout);
 
         %Backpropagation with one hidden layer 
         %Stochastic gradient descent to update weights
-        for iteration = 1:500
+        for iteration = 1:max_iterations
             sigma = 0;
             for row = 1:rows
                 X = [train(row,1:cols-1)];
                 y = train(row,cols);
-                
+               
+                %X0 is set to 1 
+                X = [1 X(1:cols-1)];
+
                 [Oh Oo] = update_output(X, Oh, Oo);
                 [Eh Eo] = backpropagate_error(y, Oh, Oo, Eh, Eo);
                 update_weights(X, Oh, Oo, Eh, Eo);
 
                 for i=1:nout
-                    sigma = sigma + 0.5*(y-Oo(i))^2;
+                    sigma = sigma + 0.5*(y-Oo(i))*(y-Oo(i));
                 end
             end
             if mod(iteration, 10) == 0
@@ -97,16 +83,16 @@ function prediction = NN(train, dev, learning_factor, weight_interval_value)
     
     function O = NN_test()
         
-        rows = size(dev)(1);
-        cols = size(dev)(2);
-        ret = zeros(1,rows-1);
-        for i = 1:rows-1
-            Oh = zeros(1,nhidden);
-            Oo = zeros(1,nout);
-            X = dev(i+1,1:cols);
+        [rows cols] = size(dev);
+        Oh = zeros(1,nhidden);
+        Oo = zeros(1,nout);
 
+        ret = zeros(1,rows);
+        for i = 1:rows
+            X = dev(i,:);
+            X = [1 X(1:cols)];
             [Oh, Oo] = update_output(X, Oh, Oo);
-            ret(i) = mean(Oo);
+            ret(i) = Oo(1);
         end
         O = ret;
     end
